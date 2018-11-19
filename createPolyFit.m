dac5af1494a6b21c4b03cc89ac2362945a532b3c
Ryan Worley <44788426+ryan-worley@users.noreply.github.com>
2018-11-19 00:41:43 -0800

    Updated Create PolyFit
    Works for both 3-4 degree polys; can enter interval over which to create polynomial fitting. Arbitrarily fits to logspace Sa values between .01g and 3g which is the area of interest in hazard curve that we need. Stores all data needed in handles variable.

--- createPolyFit.m
+++ createPolyFit.m
@@ -1,70 +1,93 @@
-function [handles] = createPolyFit(order, curve)
+function [handles] = createPolyFit(order, curve, interval, dSa)
 % Order must be defined in GUI, either 3 or 4, remember str2num
 % Remember that curve must bew defined by user via file
 
 order = str2num(order);
-loadedHazardCurve = load(curve);
 
 % Not sure how hazard curve is loaded in, assuming that each will be a row
 % in a loaded in vector, change if necessary
-LoadedSa = loadedHazardCurve(1,:);
-LoadedMAF = loadedHazardCurve(2,:);
+LoadedSa = curve(:,1);
+LoadedMAF = curve(:,2);
+npoints = length(LoadedSa);
+
+% Arbitrary Fitting Bounds
+Sa_lowerbound = .01; 
+Sa_upperbound = 3;
 
 % Use loaded and MAF to interpolate
-Sa_logspace = logspace(log10(LoadedSa), log10(Sa_hazard(length(Sa_hazard)-1)), 1000);
+Sa_logspace = logspace(log10(Sa_lowerbound), log10(Sa_upperbound), 1000);
 MAF_logspace = exp(interp1(log(LoadedSa), log(LoadedMAF), log(Sa_logspace)));
 
+% Sa for fitted curve, can be different interval than the logspace. Use
+% to find MAF values for range that we want to integrate over
+Sa_fitted = interval(1):dSa:interval(2);
+
 if order == 4
-    [p, ~] = polyfit(log(Sa_logspace),log(MAF_interpolated), 4);
+    [p, ~] = polyfit(log(Sa_logspace), log(MAF_logspace), 4);
     
-    regularpoly = exp(p(5)+p(4)*log(Sa_logspace)+p(3)*log(Sa_logspace).^2+p(2)*log(Sa_logspace).^3+p(1)*log(Sa_logspace).^4);
+    regularpoly_SSR = exp(p(5)+p(4)*log(Sa_logspace)+p(3)*log(Sa_logspace).^2+...
+        p(2)*log(Sa_logspace).^3+p(1)*log(Sa_logspace).^4);
+    
+    regularpoly = exp(p(5)+p(4)*log(Sa_fitted)+p(3)*log(Sa_fitted).^2+...
+        p(2)*log(Sa_fitted).^3+p(1)*log(Sa_fitted).^4);
+    
+    SST = sum((MAF_logspace - mean(MAF_logspace)).^2);
+    SSR = sum((MAF_logspace - regularpoly_SSR).^2);
+    R_squared = 1-SSR/SST;
     
     % Alter to reflect 4th degree of freedom
-    derivpoly = abs((p(3)+2*p(2)*log(Sa_logspace)+3*p(1)*log(Sa_logspace)...
-        .^2)./Sa_logspace.*exp(p(4)+p(3)*log(Sa_logspace)+p(2)...
-        *log(Sa_logspace).^2+p(1)*log(Sa_logspace).^3));
+    derivpoly = abs((p(5)+2*p(4)*log(Sa_fitted)+3*p(3)*log(Sa_fitted)...
+        .^2+4*p(2)*log(Sa_fitted).^3)./Sa_fitted.*exp(p(5)+p(4)*log(Sa_fitted)+p(3)...
+        *log(Sa_fitted).^2+p(2)*log(Sa_fitted).^3)+p(1)*log(Sa_fitted).^4);
+    
+    str1=['Curve: ',num2str(p(5)),' + ',num2str(p(4)),'x + ',num2str(p(3)),...
+        'x^2 + ',num2str(p(2)), 'x^3 + ', num2str(p(1)),...
+        newline 'R^2 = ', num2str(R_squared)];
     
 elseif order == 3
     [p, ~] = polyfit(log(Sa_logspace),log(MAF_interpolated), 3);
     
-    regularpoly = exp(p(4)+p(3)*log(Sa_logspace)+p(2)*log(Sa_logspace).^2+...
+    regularpoly_SSR = exp(p(4)+p(3)*log(Sa_logspace)+p(2)*log(Sa_logspace).^2+...
         p(1)*log(Sa_logspace).^3);
     
-    derivpoly = abs((p(3)+2*p(2)*log(Sa_logspace)+3*p(1)*log(Sa_logspace)...
-        .^2)./Sa_logspace.*exp(p(4)+p(3)*log(Sa_logspace)+p(2)*...
-        log(Sa_logspace).^2+p(1)*log(Sa_logspace).^3));
+    regularpoly = exp(p(4)+p(3)*log(Sa_fitted)+p(2)*log(Sa_fitted).^2+...
+        p(1)*log(Sa_fitted).^3);
+    
+    SST = sum((MAF_logspace - mean(MAF_logspace)).^2);
+    SSR = sum((MAF_interpolated - regularpoly_SSR).^2);
+    R_squared = 1-SSR/SST;
+    
+    derivpoly = abs((p(3)+2*p(2)*log(Sa_fitted)+3*p(1)*log(Sa_fitted)...
+        .^2)./Sa_fitted.*exp(p(4)+p(3)*log(Sa_fitted)+p(2)*...
+        log(Sa_fitted).^2+p(1)*log(Sa_fitted).^3));
+    
+    str1=['Curve: ',num2str(p(4)),' + ',num2str(p(3)),'x + ',num2str(p(2)),...
+        'x^2 + ',num2str(p(1)), 'x^3 ' newline 'R^2 = ', num2str(R_squared)];
 
 end
     
-    
-SST = sum((MAF_logspace - mean(MAF_logspace)).^2);
-SSR = sum((MAF_interpolated - regularpoly).^2);
-R_squared = 1-SSR/SST;
 
-handles.hazardDerivative = [Sa; derivpoly];
-handles.hazardCurve =  [Sa_logspace; regularpoly];
-
-str1=['Curve: ',num2str(p(4)),' + ',num2str(p(3)),'x + ',num2str(p(2)),'x^2 + ',num2str(p(1)), 'x^3 ' newline 'R^2 = ', num2str(R_squared)];
+handles.hazardDerivative = [Sa_fitted; derivpoly];
+handles.hazardCurve =  [Sa_fitted; regularpoly];
 
 figure
-loglog(LoadedSa, LoadedMAF, Sa_logspace, regularpoly)
+loglog(LoadedSa, LoadedMAF, Sa_fitted, regularpoly)
 title('Seismic Hazard Curve')
 xlabel('Sa')
 ylabel('Mean Annual Frequency')
 legend('Hazard Curve','Polyfit')
-xlim([Sa_logspace(1),Sa_logspace(end)])
+% xlim([Sa_fitted(1),Sa_fitted(end)])
 set(gca, ...
   'Box'         , 'off'     , ...
   'TickDir'     , 'out'     , ...
   'TickLength'  , [.02 .02] , ...
   'XMinorTick'  , 'on'      , ...
   'YMinorTick'  , 'on');
-t=text(.0015, min(Loaded_MAF), str1);
-t.FontSize = 10;
+t=text(.015, min(regularpoly), str1);
+t.FontSize = 8;
 t.BackgroundColor = 'w';
 t.EdgeColor = 'k';
 grid on
 end
-
 
 
