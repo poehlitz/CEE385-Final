f8367c92435638b3e42b5586eb54cfead7bee4b7
Ryan Worley <44788426+ryan-worley@users.noreply.github.com>
2018-12-01 14:25:03 -0800

    Collapse Fragility Change

--- FinalProject_NonGUI.m
+++ FinalProject_NonGUI.m
@@ -3,7 +3,7 @@ close all
 %% Load Hazard Curve and Fit a Polynomial in Log-Log Space
 load("HazardCurve.txt")
 
-order = '3';
+order = '4';
 curve = HazardCurve;
 interval = [0.2,3];
 dSa = 0.01;
@@ -23,10 +23,11 @@ n = 30; %Number of Groundmotions
 %output
 %I'm mainly worried about how we know which rows are which EDPs
 filenames=["Stripe1_Sa0.10_1col_S.csv","Stripe2_Sa0.35_1col_S.csv",...
-    "Stripe3..._Sa0.70_1col_S.csv","Stripe4_Sa1.05_1col_S.csv"];
+    "Stripe3_Sa0.70_1col_S.csv","Stripe4_Sa1.05_1col_S.csv"];
 stripes = [0.1,0.35,0.70,1.05];%Not sure how these are input into GUI
 
 for i=1:ns
+    disp(filenames(i))
     stripes_edp{i} = csvread(filenames(i),1,1);
 end
 
