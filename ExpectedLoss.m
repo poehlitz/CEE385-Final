bc5054fd88360243083192a7d6cb944ef8a49a92
poehlitz <43585265+poehlitz@users.noreply.github.com>
2018-12-04 15:51:00 -0800

    Started on Probability of EDP given IM

--- ExpectedLoss.m
+++ ExpectedLoss.m
@@ -4,7 +4,7 @@ function [handles] = ExpectedLoss(handles)
 handles.EDP.RIDR = 0:.01:4.0;
 
 prob_demo_edp = zeros(length(handles.EDP.RIDR),1);
-for i=1:length(handles.EDP.RIDR)
+for i=1:length(handles.EDP.RIDR) %Loop over each EDP
     prob_demo_edp = normcdf((log(handles.EDP.RIDR(i))...
         -log(handles.demo.RIDR_median))/handles.demo.RIDR_dispersion);
 end
@@ -13,6 +13,7 @@ end
 %Multiple the Probability at each EDP by probability of observing that EDP
 %at a certain IM
 
-for i=1:length(handles.hazardDerivative(1,:))
-    prob_demo_im = 
+for i=1:length(handles.hazardDerivative(1,:)) %Loop over each IM
+    %Loop over every floor
+    prob_demo_im = prob_demo_edp; %multiply by probability of RIDR at each IM at each floor
 end
--- ResponseEstimation.m
+++ ResponseEstimation.m
@@ -23,20 +23,73 @@ for j=1:length(handles.EDPnames)
 end
 
 for i=1:length(handles.EDPnames)
-    figure
-    plot(handles.stripes,handles.EDPtype.(handles.EDPnames{i}).GMData, 'Color', [0.8 0.8 0.8])
-    hold on
-    plot(handles.stripes,handles.EDPtype.(handles.EDPnames{i}).meanEDP,'LineWidth',3,'Color','r')
+    %figure
+    %plot(handles.stripes,handles.EDPtype.(handles.EDPnames{i}).GMData, 'Color', [0.8 0.8 0.8])
+    %hold on
+    %plot(handles.stripes,handles.EDPtype.(handles.EDPnames{i}).meanEDP,'LineWidth',3,'Color','r')
     %plot(stripes,medianEDP(:,i),'LineWidth',3,'Color','r')
-    title([(handles.EDPnames{i}), ' v. IM'])
-    ylabel((handles.EDPnames{i}))
-    xlabel('Sa')
-    set(gca, ...
-      'Box'         , 'off'     , ...
-      'TickDir'     , 'out'     , ...
-      'TickLength'  , [.02 .02] , ...
-      'XMinorTick'  , 'on'      , ...
-      'YMinorTick'  , 'on');
+%     title([(handles.EDPnames{i}), ' v. IM'])
+%     ylabel((handles.EDPnames{i}))
+%     xlabel('Sa')
+%     set(gca, ...
+%       'Box'         , 'off'     , ...
+%       'TickDir'     , 'out'     , ...
+%       'TickLength'  , [.02 .02] , ...
+%       'XMinorTick'  , 'on'      , ...
+%       'YMinorTick'  , 'on');
 end
 
+%Interpolate to median and dispersion of each EDP at each IM
+
+%Calculate Probability of Each EDP at Each IM
+%Requires users name EDP by IDR RIDR and PFA
+for i=1:length(handles.EDPnames)
+    if contains(handles.EDPnames{i},'IDR')
+        median_everyEDP = interp1(handles.stripes, handles.EDPtype.(handles.EDPnames{i}).medianEDP, handles.hazardDerivative(1,:)); 
+        sig_everyEDP = interp1(handles.stripes, handles.EDPtype.(handles.EDPnames{j}).variationEDP, handles.hazardDerivative(1,:));
+        handles.EDPtype.(handles.EDPnames{i}).prob_im = (1./(handles.EDP.IDR.*sig_everyEDP*sqrt(2*pi))).*exp(-((log(handles.EDP.IDR)-log(median_everyEDP)).^2)./(2*(sig_everyEDP.^2)));
+        figure
+        plot(handles.EDP.IDR,handles.EDPtype.(handles.EDPnames{i}).prob_im)
+            title(['Probability Distribution of ',(handles.EDPnames{i})])
+        ylabel(['P(',(handles.EDPnames{i}),')'])
+        xlabel('Sa')
+        set(gca, ...
+          'Box'         , 'off'     , ...
+          'TickDir'     , 'out'     , ...
+          'TickLength'  , [.02 .02] , ...
+          'XMinorTick'  , 'on'      , ...
+          'YMinorTick'  , 'on');
+    elseif contains(handles.EDPnames{i},'RIDR')
+        median_everyEDP = interp1(handles.stripes, handles.EDPtype.(handles.EDPnames{i}).medianEDP, handles.EDP.IDR); 
+        sig_everyEDP = interp1(handles.stripes, handles.EDPtype.(handles.EDPnames{j}).variationEDP, handles.EDP.IDR);
+        handles.EDPtype.(handles.EDPnames{i}).prob_im = (1./(handles.EDP.RIDR.*sig_everyEDP*sqrt(2*pi))).*exp(-((log(handles.EDP.RIDR)-log(median_everyEDP).^2)./(2*(sig_everyEDP.^2))));
+                figure
+        plot(handles.EDP.IDR,handles.EDPtype.(handles.EDPnames{i}).prob_im)
+            title(['Probability Distribution of ',(handles.EDPnames{i})])
+        ylabel(['P(',(handles.EDPnames{i}),')'])
+        xlabel('Sa')
+        set(gca, ...
+          'Box'         , 'off'     , ...
+          'TickDir'     , 'out'     , ...
+          'TickLength'  , [.02 .02] , ...
+          'XMinorTick'  , 'on'      , ...
+          'YMinorTick'  , 'on');
+    elseif contains(handles.EDPnames{i},'PFA')
+        median_everyEDP = interp1(handles.stripes, handles.EDPtype.(handles.EDPnames{i}).medianEDP, handles.EDP.IDR); 
+        sig_everyEDP = interp1(handles.stripes, handles.EDPtype.(handles.EDPnames{j}).variationEDP, handles.EDP.IDR);
+        handles.EDPtype.(handles.EDPnames{i}).prob_im = (1./(handles.EDP.PFA.*sig_everyEDP*sqrt(2*pi))).*exp(-((log(handles.EDP.PFA)-log(median_everyEDP).^2)./(2*(sig_everyEDP.^2))));
+        figure
+        plot(handles.EDP.IDR,handles.EDPtype.(handles.EDPnames{i}).prob_im)
+            title(['Probability Distribution of ',(handles.EDPnames{i})])
+        ylabel(['P(',(handles.EDPnames{i}),')'])
+        xlabel('Sa')
+        set(gca, ...
+          'Box'         , 'off'     , ...
+          'TickDir'     , 'out'     , ...
+          'TickLength'  , [.02 .02] , ...
+          'XMinorTick'  , 'on'      , ...
+          'YMinorTick'  , 'on');
+    else
+        disp('error')
+    end
 end
