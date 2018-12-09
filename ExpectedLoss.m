d2d31e376730343121b846145abb4fbcdde941ea
poehlitz <43585265+poehlitz@users.noreply.github.com>
2018-12-09 15:16:31 -0800

    Changed RIDR to peak of all stories

--- ExpectedLoss.m
+++ ExpectedLoss.m
@@ -4,22 +4,18 @@ handles.demo.RIDR_median = 0.015; % User input here
 handles.demo.RIDR_dispersion = 0.3; % User input here
 
 %Probability of Demolition At Each EDP
-prob_demo_edp = zeros(length(handles.EDP.RIDR),1);
-for i=1:length(handles.EDP.RIDR) %Loop over each EDP
-    prob_demo_edp(i) = normcdf((log(handles.EDP.RIDR(i))...
+%Loop over each EDP
+    prob_demo_edp = normcdf((log(handles.EDP.RIDR)...
         -log(handles.demo.RIDR_median))/handles.demo.RIDR_dispersion);
-end
 
 %Probability of Demolition at Each IM
 %Multiple the Probability at each EDP by probability of observing that EDP
 %at a certain IM
 Sa = handles.hazardDerivative(1,:);
-prob_demo_im = zeros(handles.numStory - 1, length(Sa));
+prob_demo_im = zeros(1, length(Sa));
 
 for i=1:length(Sa) %Loop over each IM
-    %Loop over every floor, this isn't finished yet huh
-        story_index = strcat('RIDR', num2str(handles.numStory - j));
-        prob_demo_im(i) = trapz(handles.EDP.RIDR, prob_demo_edp.*handles.EDPtype.(story_index).pdf_edp_im(:, i)); %multiply by probability of RIDR at each IM at each floor
+        prob_demo_im(i) = trapz(handles.EDP.RIDR, prob_demo_edp'.*handles.EDPtype.RIDR1.pdf_edp_im(:, i)); %multiply by probability of RIDR at each IM at each floor
 end
 
 handles.demo.p_im = prob_demo_im;
